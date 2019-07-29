6ca3c73eb60cc33f45f2b0627efc3e7e3391c0a6
Secret608 <2289544554@qq.com>
2019-07-30 00:18:56 +0800

    优化alter

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,8 @@
+//运行
 $(function () {
     $(".demo>.btn-success").click(function () {
         var Name = $(this).attr("id");
-
+        var tip = $("#tip");
         $.ajax({
             async: false,//同步，异步
             url: "/index/run", //请求的服务端地址
@@ -12,24 +13,36 @@ $(function () {
             dataType: "text",
             success: function (data) {
                 //成功之后的处理，返回的数据就是 data
-                $(this).text("运行中");
-                alert('success');
+                tip.attr("class","alter_a");
+                tip.text("开启运行");
+                // $.parser.parse(tip);
+                setTimeout(function () {tip.attr("class","alter_0");}, 1000);
+                // $.parser.parse(tip);
             },
             error: function () {
-                alert('error'); //错误的处理
+
+                tip.attr("class","alter_b");
+                tip.text("运行异常");
+                // $.parser.parse(tip);
+                setTimeout(function () {tip.attr("class","alter_0");}, 1000);
+                // $.parser.parse(tip);
             }
         });
     });
 });
 
+//先关闭接口
 $(function () {
-    $(".demo>.badge-light").click(function () {
+    $(".demo>.badge-light, .demo>.btn-danger, .card-header div .btn-danger , .card-header div .btn-success ").click(function () {
         alert('对不起，您没有相应权限，请联系管理员（xiaofeng.zhang@163.com）'); //错误的处理
     });
 });
 
+//报告跳转
 $(function () {
-    $(".demo>.btn-danger").click(function () {
-        alert('对不起，您没有相应权限，请联系管理员（xiaofeng.zhang@163.com）'); //错误的处理
+    $(".demo>.btn-info").click(function () {
+        window.open("/static/Report/Demo.html");
     });
 });
+
+
